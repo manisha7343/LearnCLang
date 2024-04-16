b0cea285278be8ddef65ce83fe8b52ab62ceb5b1
Manisha Sharma <166391383+manisha7343@users.noreply.github.com>
2024-04-16 12:27:36 +0530

    Create 2-InteringName.c

---
+++ 2-InteringName.c
@@ -0,0 +1,18 @@
+#include <stdoo.h>
+#include <conio.h>
+
+ void main(){
+   char name[20];
+
+   clrdcr();
+//----------------------------------------------------
+
+   
+   printf("enter you name:\n);
+   scanf("%s"&name);
+   printf("your nam eis %s.", name); 
+
+   
+ //--------------------------------------------------
+   getch();
+}
